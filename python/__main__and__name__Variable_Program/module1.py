6a1cdbada814731ec76f3d545dc812539b4d5b7b
KARTHIKEYAN GANESH <37135347+kn123g@users.noreply.github.com>
2020-04-04 23:40:37 +0530

    Add files via upload

---
+++ python/__main__and__name__Variable_Program/module1.py
@@ -0,0 +1,6 @@
+''' __main__   and  __name__  variable '''
+from module2 import *
+if __name__ == "__main__" :
+    print("HAI LETS START MODULE1 AS MAIN")
+if __name__ != "__main__" :
+    print(__name__)
---
+++ python/__main__and__name__Variable_Program/module2.py
@@ -0,0 +1,8 @@
+''' __main__   and  __name__  variable '''
+from module1 import *
+
+if __name__ == "__main__" :
+    print("HAI LETS START MODULE2 AS MAIN")
+if __name__ != "__main__" :
+    print(__name__)
+
