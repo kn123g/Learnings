a32cbefaf529ad8c8fdb67b2374d77111e7de249
ganeshk <karthiknganesh96@gmail.com>
2020-04-05 13:49:29 +0530

    test file updated

--- python/test.py
+++ python/test.py
@@ -1,1 +1,10 @@
-print("hai")
+from socket import *
+s = socket()
+s.bind(("localhost", 9999))
+s.listen(3)
+while True:
+    c, address = s.accept()
+    print("Connected to ", address)
+    c.send(bytes("Server welcomes you", "utf-8"))
+
+    c.close()
